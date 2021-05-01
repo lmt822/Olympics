c6752c500271e38009e5fbf8a93eedd1903d5427
mchen08 <79431225+mchen08@users.noreply.github.com>
2021-05-01 16:17:17 -0700

    add three headers

---
+++ client/src/components/Headers/AverageHeader.js
@@ -0,0 +1,107 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container,
+        Card,
+        Row,
+        Col,
+        Carousel,
+        CarouselItem,
+        CarouselIndicators,
+        CarouselCaption} from "reactstrap";
+
+const items = [
+  {
+    src: require("assets/img/averageheader.jpg").default,
+    altText: "Somewhere",
+    caption: ""
+  },
+];
+
+// core components
+function CountryHeader() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [animating, setAnimating] = React.useState(false);
+  const onExiting = () => {
+    setAnimating(true);
+  };
+  const onExited = () => {
+    setAnimating(false);
+  };
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+      >
+          <Row>
+            <Col className="ml-auto mr-auto">
+              <Card className="page-carousel">
+                <Carousel
+                  activeIndex={activeIndex}
+                  next={next}
+                  previous={previous}
+                >
+                  <CarouselIndicators
+                    items={items}
+                    activeIndex={activeIndex}
+                    onClickHandler={goToIndex}
+                  />
+                  {items.map((item) => {
+                    return (
+                      <CarouselItem
+                        onExiting={onExiting}
+                        onExited={onExited}
+                        key={item.src}
+                      >
+                        <img src={item.src} alt={item.altText} />
+                        <CarouselCaption
+                          captionText={item.caption}
+                          captionHeader="Average Number of Medals per Athlete"
+                        />
+                      </CarouselItem>
+                    );
+                  })}
+
+                </Carousel>
+              </Card>
+            </Col>
+          </Row>
+      </div>{" "}
+    </>
+  );
+}
+
+export default CountryHeader;
